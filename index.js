57fd9e40ded9da12493807bfa94035375195fa80
OgmoTVTwitch <65562806+OgmoTVTwitch@users.noreply.github.com>
2020-05-18 22:11:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ bot.login(TOKEN);
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 };
+
+{ if(msg.content.starsWith('!info')) {
+ eslseif (msg.mentions.users.size)  {
+    const taggedUser = msg.mentions.users.first()
+    msg.channel.send(`Voici les info de: ${taggedUser.username}`);
+ } else {
+   msg.reply('Please tage a valid user! ');
+  }
+}  
+
+module.exports = {
+  Info: require('./info'),
+  name: 'info',
+  description: 'Info!',
+  execute(msg, args) {
+    msg.reply('Voici les info de');
+    msg.channel.send('Voici les info de');
+  },
+};
+       
